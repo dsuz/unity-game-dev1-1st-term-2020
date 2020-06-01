0e5ca1bf58a8b36da7075eb97edb4663ca47942a
Daisuke Suzuki <bboydaisuke@gmail.com>
2020-06-02 04:15:34 +0900

    2週目の内容を追加した

---
+++ Assets/Scripts/BallController.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// ボールを制御するコンポーネント。ボールのオブジェクトに追加して使う。
+/// </summary>
+[RequireComponent(typeof(Rigidbody2D))]
+public class BallController : MonoBehaviour
+{
+    /// <summary>ボールが最初に動く方向</summary>
+    [SerializeField] Vector2 m_powerDirection = Vector2.up + Vector2.right;
+    /// <summary>ボールを最初に動かす力の大きさ</summary>
+    [SerializeField] float m_powerScale = 5f;
+    Rigidbody2D m_rb2d;
+
+    void Start()
+    {
+        m_rb2d = GetComponent<Rigidbody2D>();
+        // ボールを動かす
+        Push();
+    }
+
+    void Update()
+    {
+
+    }
+
+    /// <summary>
+    /// ボールに力を加える
+    /// </summary>
+    public void Push()
+    {
+        m_rb2d.AddForce(m_powerDirection.normalized * m_powerScale, ForceMode2D.Impulse);
+    }
+}
---
+++ Assets/Scripts/PaddleController.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// パドルを制御するコンポーネント。パドルのオブジェクトに追加して使う。
+/// </summary>
+[RequireComponent(typeof(Rigidbody2D))]
+public class PaddleController : MonoBehaviour
+{
+    /// <summary>パドルの移動速度</summary>
+    [SerializeField] float m_speed = 5f;
+    Rigidbody2D m_rb2d;
+
+    void Start()
+    {
+        m_rb2d = GetComponent<Rigidbody2D>();
+    }
+
+    void Update()
+    {
+        // 水平方向の入力を検出する
+        float h = Input.GetAxisRaw("Horizontal");
+        // 入力に応じてパドルを水平方向に動かす
+        m_rb2d.velocity = h * Vector2.right * m_speed;
+    }
+}
