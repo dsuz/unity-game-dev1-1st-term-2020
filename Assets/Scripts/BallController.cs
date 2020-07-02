72fce37d7b565a53abaed966cc379cfe12cb3d70
Daisuke Suzuki <bboydaisuke@gmail.com>
2020-07-02 13:29:10 +0900

    「音」の部分を追加・更新した

--- Assets/Scripts/BallController.cs
+++ Assets/Scripts/BallController.cs
@@ -33,4 +33,31 @@ public class BallController : MonoBehaviour
     {
         m_rb2d.AddForce(m_powerDirection.normalized * m_powerScale, ForceMode2D.Impulse);
     }
+
+    /// <summary>
+    /// ボールがコライダーに衝突した時に呼ばれる関数
+    /// </summary>
+    /// <param name="collision">衝突情報</param>
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        AudioSource audio = this.gameObject.GetComponent<AudioSource>();    // この GameObject から AudioSource コンポーネントを取ってくる
+        if (audio != null)  // コンポーネントが取れたら
+        {
+            audio.Play();   // AudioSource コンポーネントの AudioClip プロパティにアサインされている音を鳴らす
+        }
+    }
+
+    /// <summary>
+    /// ボールがトリガーに接触した時に呼ばれる関数
+    /// </summary>
+    /// <param name="collision">接触情報</param>
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        // OnCollisionEnter2D と同じの処理をする
+        AudioSource audio = this.gameObject.GetComponent<AudioSource>();
+        if (audio != null)
+        {
+            audio.Play();
+        }
+    }
 }
